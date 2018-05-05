2deeb8415b4f5021f16d8ce6fe33fa00b8bcef30
Simon Zheng <simz97@outlook.com>
2018-05-05 00:50:31 -0400

    More refactoring and added values

--- unminer.js
+++ unminer.js
@@ -6,6 +6,11 @@ const h = squareSize * boardSize;
 let canvas, context;
 const board = Array(boardSize);
 
+function cmpClr(a, b) {
+    if (a == b) return true;
+    return a.toString() === b.toString();
+}
+
 function preload() {}
 
 function setup() {
@@ -17,7 +22,7 @@ function setup() {
     for (let i = 0; i < w; i++) {
         board[i] = Array(boardSize);
         for (let j = 0; j < h; j++) {
-            board[i][j] = "lightblue";
+            board[i][j] = { col: color("lightblue"), val: Math.round(Math.random() * 8.5) };
         }
     }
 
@@ -34,8 +39,13 @@ function draw() {
 function drawBoard() {
     for (let i = 0, x = 0; i < w; i+=squareSize, x++) {
         for (let j = 0, y = 0; j < h; j+=squareSize, y++) {
-            fill(color(board[x][y]));
+            fill(board[x][y].col);
             rect(i, j, squareSize, squareSize, squareRadius);
+            if (cmpClr(board[x][y].col, color("lightgrey"))) {
+                fill(color("green"));
+                textSize(46);
+                text(' '+board[x][y].val, i, j, squareSize, squareSize);
+            }
         }
     }
 }
@@ -47,8 +57,22 @@ function mousePressed() {
     if (x < w && y < h) {
         let i = x/squareSize;
         let j = y/squareSize;
-        if (mouseButton === LEFT) board[i][j] = "lightgrey";
-        if (mouseButton === RIGHT && board[i][j] !== "lightgrey") board[i][j] = "red";
+
+        if (mouseButton === LEFT && !cmpClr(board[i][j].col, color("red"))) {
+            board[i][j].col = color("lightgrey");
+            fill(board[i][j].col);
+        }
+
+        if (mouseButton === RIGHT) {
+            if (cmpClr(board[i][j].col, color("red"))) {
+                board[i][j].col = color("lightblue");
+                fill(board[i][j].col);
+            }
+            else if (!cmpClr(board[i][j].col, color("lightgrey"))) {
+                board[i][j].col = color("red");
+                fill(board[i][j].col);
+            }
+        }
 
         rect(x, y, squareSize, squareSize, squareRadius);
     }
