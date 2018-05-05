35d8cdb190bc6b75f2c1f347143f581b0fb768fb
Simon Zheng <simz97@outlook.com>
2018-05-04 21:21:38 -0400

    Lots of fixes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # unminer
-Minesweeper game in JavaScript using P5 and P5.play library/framework/engine.
+Minesweeper game in JavaScript using P5.play library.
+
+## Instructions
+
+- Download the [P5.play library](http://p5play.molleindustria.org/)
+- Extract it inside this repo's root folder
+- Run `npm install` then `npm start` inside the extracted folder
--- unminer.js
+++ unminer.js
@@ -5,28 +5,21 @@ const w = squareSize * boardSize;
 const h = squareSize * boardSize;
 let canvas, context;
 const board = Array(boardSize);
-// let x, y = -1;
-// let c;
-// let square;
 
 function preload() {}
 
 function setup() {
     createCanvas(w+5, h+5);
     canvas = document.querySelector("canvas");
+    canvas.oncontextmenu = () => { return false; }
     context = canvas.getContext("2d");
 
-    // for (let i = 0; i < w; i++) {
-    //     board[i] = Array(boardSize);
-    // }
-
-    // for (let i = 0; i < w; i++) {
-    //     for (let j = 0; j < h; j++) {
-    //         board[i][j] = createSprite(i+squareSize, j+squareSize, squareSize, squareSize);
-    //         board[i][j].shapeColor = color("lightblue");
-    //         board[i][j].immovable = true;
-    //     }
-    // }
+    for (let i = 0; i < w; i++) {
+        board[i] = Array(boardSize);
+        for (let j = 0; j < h; j++) {
+            board[i][j] = "lightblue";
+        }
+    }
 
     drawBoard();
 
@@ -39,31 +32,33 @@ function draw() {
 }
 
 function drawBoard() {
-    for (let i = 0; i < w; i+=squareSize) {
-        for (let j = 0; j < h; j+=squareSize) {
-            // if (i === x && j === y) {
-            //     fill(c);
-            //     rect(i, j, squareSize, squareSize, squareRadius);
-            //     continue;
-            // }
-            fill(color("lightblue"));
+    for (let i = 0, x = 0; i < w; i+=squareSize, x++) {
+        for (let j = 0, y = 0; j < h; j+=squareSize, y++) {
+            fill(color(board[x][y]));
             rect(i, j, squareSize, squareSize, squareRadius);
         }
     }
 }
 
-function mouseClicked() {
+function mousePressed() {
     let x = Math.round(mouseX/squareSize-0.5)*squareSize;
     let y = Math.round(mouseY/squareSize-0.5)*squareSize;
+
+    if (x < w && y < h) {
+        let i = x/squareSize;
+        let j = y/squareSize;
+        if (mouseButton === LEFT) board[i][j] = "lightgrey";
+        if (mouseButton === RIGHT && board[i][j] !== "lightgrey") board[i][j] = "red";
+
+        rect(x, y, squareSize, squareSize, squareRadius);
+    }
 
     clear();
     redraw();
 
-    if (x < w && y < h) {
-        if (mouseButton === LEFT) fill(color("lightgrey"));
-        if (mouseButton === RIGHT) fill(color("red"));
-        rect(x, y, squareSize, squareSize, squareRadius);
-    }
+    return false;
+}
 
+function f() {
     return false;
 }
